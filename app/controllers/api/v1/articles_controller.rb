d61eba938b3bddcb7b739fd58b2dbf197b64216c
khanhtt2 <khanhtt2@topica.edu.vn>
2019-05-27 15:04:09 +0700

    add

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -10,6 +10,7 @@ class Api::V1::ArticlesController < ApplicationController
     puts request.remote_ip
     puts request.host
     puts request.original_url
+    puts request.request_url
     #url = "http://localhost:3000/api/lp/all"
     #@result = RestClient.get(url, headers={})
   end
