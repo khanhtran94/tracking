bc96476397e4f95009d0ef7a982191d7d267ecf7
khanhtt2 <khanhtt2@topica.edu.vn>
2019-05-27 10:32:15 +0700

    tracking

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   #resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
   namespace :api do
     namespace :v1 do
      resources :articles
     end
   end
+
 end
