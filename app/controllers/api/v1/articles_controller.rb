2a61cac51a9970116f22c780804176b030e0d506
khanhtt2 <khanhtt2@topica.edu.vn>
2019-05-27 11:48:45 +0700

    add

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::ArticlesController < ApplicationController
   # GET /articles.json
   def index
     #binding.pry
-    puts request
+    puts request.remote_url
     #url = "http://localhost:3000/api/lp/all"
     #@result = RestClient.get(url, headers={})
   end
